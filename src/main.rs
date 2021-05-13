77f6a800305717b58c6ffc65d3dc39a342972964
Ryan Orlando <jsrco@hotmail.com>
2021-05-13 00:51:32 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+// got a new machine. just doing the entire book to relearn
+    let mut your_name = String::new();
+    println!("Hello, what is your name?");
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    println!("Hello, {}", your_name);
 }
