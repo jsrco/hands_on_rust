8a9567b9a876950f4a5d11be58c430f672ab1fb9
Ryan Orlando <jsrco@hotmail.com>
2021-05-19 22:56:27 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
 use std::io::stdin;
 
-fn main() {
-// got a new machine. just doing the entire book to relearn
-    let mut your_name = String::new();
-    println!("Hello, what is your name?");
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+fn get_str_in() -> String {
+    let mut user_in = String::new();
     stdin()
-        .read_line(&mut your_name)
+        .read_line(&mut user_in)
         .expect("Failed to read line");
-    println!("Hello, {}", your_name);
+    user_in.trim().to_lowercase()
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = get_str_in();
+    let mut allow_them_in = false;
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
+    }
 }
